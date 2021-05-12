607edae1f216a9315f53d514018b0e2ea23c58bc
xzaliman <83049855+xzaliman@users.noreply.github.com>
2021-05-12 19:35:38 +0700

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -35,9 +35,9 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByEmcee.png"
+    fname2 = "LogoByxz5pd.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Created By Emcee✨")
+    await tbot.send_file(event.chat_id, fname2, caption="Created By @GrupCinta")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -75,13 +75,13 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByEmcee.png"
+    fname2 = "LogoByxz5pd.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Created By Emcee")
+    await tbot.send_file(event.chat_id, fname2, caption="Created By @GrupCinta")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Emcee_Support, {e}')
+   await event.reply(f'Error Report @GrupCinta, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")

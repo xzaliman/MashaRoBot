c35fc041bdd53d3e31ea1ec61e1b865063ca0dc0
xzaliman <83049855+xzaliman@users.noreply.github.com>
2021-05-15 11:21:58 +0700

    Update __google.py

--- MashaRoBot/modules/__google.py
+++ MashaRoBot/modules/__google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "GOOGLE"
+__mod_name__ = "Google"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
